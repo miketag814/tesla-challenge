ba7db34a5ff13af5d76c4d867867f7da6e7afd5a
Michael Tagliaferro <miketag814@gmail.com>
2022-10-15 14:53:42 -0400

    props added. Can't change bg image.

--- tesla-challenge/src/components/Section.js
+++ tesla-challenge/src/components/Section.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Section.css'
 
-function Section() {
+function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
     return (
         <div className='section'>
             <div className='section_text'>
-                <h1>Model-S</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </div>
 
             <div className='section_button'>
                 <div className='button_group'>
-                    <button className='customOrderButton'>Custom Order</button>
-                    <button className='existingInventoryButton'>Existing Inventory</button>
+                    <button className='customOrderButton'>{leftBtnText}</button>
+                    <button className='existingInventoryButton'>{rightBtnText}</button>
                 </div>
                 <svg className='section_arrow' xmlns='http://www.w3.org/2000/svg' fill="white" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg>
             </div>
